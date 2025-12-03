77d1dada84dea5c6fd55fea8d10cd337954cfe53
lillecarl <git@lillecarl.com>
2025-12-03 03:24:25 +0100

    add disko input

--- flake.nix
+++ flake.nix
@@ -1,21 +1,23 @@
 {
   inputs = {
-    # flake-compatish.url = "github:lillecarl/flake-compatish/main";
     flake-compatish = {
-      #url = "path:/home/lillecarl/Code/flake-compatish";
       url = "github:lillecarl/flake-compatish";
       flake = false;
     };
     nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
     home-manager = {
       url = "github:nix-community/home-manager/master";
-      #url = "path:/home/lillecarl/Code/home-manager";
       inputs.nixpkgs.follows = "nixpkgs";
     };
     catppuccin = {
       url = "github:catppuccin/nix";
       inputs.nixpkgs.follows = "nixpkgs";
     };
+    disko = {
+      url = "github:nix-community/disko";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
   };
-  outputs = inputs: {};
+  outputs = inputs: {
+  };
 }
