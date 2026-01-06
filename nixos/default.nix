37d1ed2995b3f8bbe7b650a0829221fe5bba2d12
lillecarl <git@lillecarl.com>
2026-01-06 18:27:10 +0100

    binfmt to support building for alternative platform(s)

--- nixos/default.nix
+++ nixos/default.nix
@@ -17,6 +17,15 @@
     boot.loader.grub.enable = true;
     boot.initrd.availableKernelModules = [ "ahci" "xhci_pci" "virtio_scsi" ];
     boot.kernelPackages = pkgs.linuxPackages_latest;
+    # Support building crossPlatform with QEMU
+    boot.binfmt.emulatedSystems = [
+      {
+        "x86_64-linux" = "aarch64-linux";
+        "aarch64-linux" = "x86_64-linux";
+      }
+      .${pkgs.stdenv.hostPlatform.system}
+    ];
+    # Terminfo packages for terminals we're using
     environment.systemPackages = with pkgs.pkgsBuildBuild; [
       foot.terminfo
       tmux.terminfo
