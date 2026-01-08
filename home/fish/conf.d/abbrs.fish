c495161d08e919eed32e012b10ab1cd0ba3373b9
lillecarl <git@lillecarl.com>
2026-01-08 23:53:24 +0000

    fish: add common abbreviations

---
+++ home/fish/conf.d/abbrs.fish
@@ -0,0 +1,8 @@
+#! /usr/bin/env fish
+
+abbr --add -- jc 'journalctl --unit'
+abbr --add -- jcu 'journalctl --user --unit'
+abbr --add -- kc kubectl
+abbr --add -- kns kubens
+abbr --add -- sc 'sudo systemctl'
+abbr --add -- scu 'systemctl --user'
