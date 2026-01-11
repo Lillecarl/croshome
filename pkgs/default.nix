e900aac94e2aeee95912502b0392660cbfa3428a
lillecarl <git@lillecarl.com>
2026-01-11 17:38:30 +0000

    use tmuxp to to open both waypipe and ssh port forwarding

--- pkgs/default.nix
+++ pkgs/default.nix
@@ -1,37 +1,102 @@
-self: pkgs: {
-  foot = pkgs.foot.overrideAttrs (pattrs: {
+final: prev: {
+  foot = prev.foot.overrideAttrs (pattrs: {
     patches = pattrs.patches or [ ] ++ [
       ./0001-ignore-numlock.patch
     ];
   });
 
-  autowaypipe = self.writeShellApplication {
-    name = "autowaypipe";
-    runtimeInputs = [
-      self.coreutils
-      self.openssh
-      self.waypipe
-    ];
-    text = # bash
-      ''
-        set -euo pipefail
 
-        delay=1
-        max_delay=600  # 10 minutes
-
-        while true; do
-            if waypipe --compress zstd --display wayland-1 --no-gpu ssh lillecarl@65.108.150.98; then
-                delay=1
-                sleep "$delay"
-            else
-                echo "Failed, waiting ''${delay}s"
-                sleep "$delay"
-                delay=$((delay * 2))
-                if [ "$delay" -gt "$max_delay" ]; then
-                    delay=$max_delay
-                fi
-            fi
-        done
-      '';
+  tmux-unscroll = prev.tmux.overrideAttrs {
+    src = /home/lillecarl/Code/tmux;
   };
+
+  hetztop-forward =
+    let
+      sessionConfig = prev.writeText "tmuxp.yaml" (
+        builtins.toJSON {
+          session_name = "hetztop-forwards";
+          windows = [
+            {
+              window_name = "scripts";
+              layout = "even-vertical";
+              panes = [
+                {
+                  shell_command = final.lib.getExe (final.writeShellApplication {
+                    name = "hetztop-waypipe";
+                    runtimeInputs = [
+                      final.coreutils
+                      final.openssh
+                      final.waypipe
+                    ];
+                    text = # bash
+                      ''
+                        set -x
+
+                        delay=1
+                        max_delay=600  # 10 minutes
+
+                        while true; do
+                            if waypipe --unlink-socket --compress zstd --display wayland-1 --no-gpu ssh lillecarl@65.108.150.98; then
+                                delay=1
+                                sleep "$delay"
+                            else
+                                echo "Failed, waiting ''${delay}s"
+                                sleep "$delay"
+                                delay=$((delay * 2))
+                                if [ "$delay" -gt "$max_delay" ]; then
+                                    delay=$max_delay
+                                fi
+                            fi
+                        done
+                      '';
+                  });
+                }
+                {
+                  shell_command = final.lib.getExe (final.writeShellApplication {
+                    name = "hetztop-ports";
+                    runtimeInputs = [
+                      final.coreutils
+                      final.openssh
+                      final.waypipe
+                    ];
+                    text = # bash
+                      ''
+                        set -x
+
+                        delay=1
+                        max_delay=600  # 10 minutes
+
+                        while true; do
+                            if ssh -L 8000:localhost:8000 lillecarl@65.108.150.98; then
+                                delay=1
+                                sleep "$delay"
+                            else
+                                echo "Failed, waiting ''${delay}s"
+                                sleep "$delay"
+                                delay=$((delay * 2))
+                                if [ "$delay" -gt "$max_delay" ]; then
+                                    delay=$max_delay
+                                fi
+                            fi
+                        done
+                      '';
+                  });
+                }
+              ];
+            }
+          ];
+        }
+      );
+    in
+    final.writeShellApplication {
+      name = "hetztop-forwards";
+      runtimeInputs = [
+        final.tmuxp
+      ];
+      text = # bash
+        ''
+          set -euo pipefail
+          tmuxp load ${sessionConfig}
+        '';
+    };
 }
