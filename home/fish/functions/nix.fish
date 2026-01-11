27ca8f765ed6e453c52ffbce276f5672050adfd6
lillecarl <git@lillecarl.com>
2026-01-11 17:38:30 +0000

    rework nix wrapper

--- home/fish/functions/nix.fish
+++ home/fish/functions/nix.fish
@@ -1,24 +1,19 @@
 function nix
+    if test -z "$argv"
+        command nix
+        return
+    end
+
     # Extract nix subcommand
     set command $argv[1]
-    set argvClone $argv
-    set --erase argv[1]
 
     set --export NIX_CONFIG "access-tokens = github.com=$(echo $PS_GHBASIC | base64 -d)"
 
     if test $command = build
-        command nix build --no-link --print-out-paths --impure $argv
-    else if test $command = repl && test -z "$argv"
-        command nix repl --file $FLAKE/repl.nix
+        command nix build --no-link --print-out-paths --impure $argv[2..-1]
     else if test $command = run
-        command nix run --impure $argv
-    else if test $command = getrev
-        set input nixpkgs
-        if test -n "$argv[1]"
-            set input $argv[1]
-        end
-        command nix eval --expr "(import $FLAKE).inputs.$input.rev" --impure --raw
+        command nix run --impure $argv[2..-1]
     else
-        command nix $command $argv
+        command nix $argv
     end
 end
